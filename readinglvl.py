d1c3561cd24912e9aaa190634d8d9f3c29dd311e
Stefano <sena.stefano@gmail.com>
2022-10-28 10:25:18 -0400

    Organized and redone for any codespace.

---
+++ readinglvl.py
@@ -0,0 +1,58 @@
+import re
+
+
+def main():
+    # Variables
+    letters = 0
+    words = 1
+    sentences = 0
+
+    # Input
+    text = input("Input your text: ")
+    if not s.isalpha():
+        print("Please enter only alphabetical characters for your name.")
+
+    # Count letters
+    letters = len(text) - text.count(" ") - text.count(".") - text.count("!") - text.count("?")
+    # for l in range(len(text)):
+    #     if text[l].isalpha():
+    #         letters += 1
+
+    # Count words
+    # words = len(re.findall(r"\w+", text))
+    for i in range(len(text)):
+        if (i == 0 and text[i] != " ") or (i != len(text) - 1 and text[i] == " " and text[i + 1] != " "):
+            words += 1
+
+    # Count Sentences
+    for s in text:
+        if s.count("."):
+            sentences += 1
+        if s.count("?"):
+            sentences += 1
+        if s.count("!"):
+            sentences += 1
+
+    # Equation + new variables for equation
+    L = letters / words * 100
+    S = sentences / words * 100
+    # cl_index = 0.0588 * L - 0.296 * S - 15.8
+    index = round(0.0588 * L - 0.296 * S - 15.8)
+
+    # Print checks
+    # print(f"letters: {letters}")
+    # print(f"words: {words}")
+    # print(f"sentences: {sentences}")
+
+    # Print grade statements
+    if index < 1:
+        print("Grade: Before Grade 1.")
+    elif index >= 16:
+        print("Grade 16+")
+    else:
+        print(f"Grade: {index}")
+
+
+
+if __name__ == "__main__":
+    main()
